47f76d689d6e414e5f9324156054a1f34af95897
jay0425 <heedev94@gmail.com>
2024-02-22 19:50:50 +0900

    사이드바, 검색창 UI 구현

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const API_KEY = `b3bcbacde44c40eaaf12b35006d52252`;
 let newsList = [];
+const menus = document.querySelectorAll('.menus button');
+menus.forEach((menu) => menu.addEventListener('click', (event) => getNewsByCategory(event)));
 
 const getLatestNews = async () => {
-  // const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
   // const url = new URL(`https://lovely-hummingbird-ed40c1.netlify.app/top-headlines?category=science`);
   // const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
-  let url = new URL(`https://lovely-hummingbird-ed40c1.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+  // let url = new URL(`https://lovely-hummingbird-ed40c1.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
 
   console.log('uuu', url);
 
@@ -16,6 +18,10 @@ const getLatestNews = async () => {
   console.log('ddd', newsList);
 
   console.log('rrr', response);
+};
+
+const getNewsByCategory = (event) => {
+  console.log('catagory');
 };
 
 const render = () => {
@@ -42,3 +48,28 @@ const render = () => {
 };
 
 getLatestNews();
+
+// 1. 버튼들에 클릭 이벤트를 준다.
+// 2. 카테고리 별 뉴스를 가져온다.
+// 3. 가져온 뉴스를 보여준다.
+
+// 사이드 바
+
+const openNav = () => {
+  document.getElementById('mySidenav').style.width = '250px';
+};
+
+const closeNav = () => {
+  document.getElementById('mySidenav').style.width = '0';
+};
+
+// 검색창
+
+const openSearchBox = () => {
+  let inputArea = document.getElementById('input-area');
+  if (inputArea.style.display === 'inline') {
+    inputArea.style.display = 'none';
+  } else {
+    inputArea.style.display = 'inline';
+  }
+};
